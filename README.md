761c4da96dbc9f26f0e6122fe2f9d9ea2e6bf373
Rebecca Reyes <rebreyaz@gmail.com>
2023-01-14 01:05:53 -0700

    install npm body parser

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ STEP 4: require express in your calculator.js file.
 STEP 5: run the server using nodemon in the terminal $ nodemon calculator.js
 
 STEP 6: create an index.html file and send the entire index.html file when the browser tries to access the home route by using res.sendFile() in the calculator.js file.
+
+STEP 7: install npm body parser in the terminal
+$ npm install body-parser
+This will allow us to pass information that we get sent from the post request.
+
+STEP 8: Require the npm body parser in the calculator.js file. 
+By using a Body Parser, we're able to parse the HTTP request that we get and by using urlencoded we can get access to the form data, and we can then tap into each of form entries as if they were just properties of the object body. 
--- calculator.js
+++ calculator.js
@@ -1,22 +1,37 @@
 // STEP 1: require express.js
 const express = require("express");
 
+// STEP 8: require npm body-parser
+const bodyParser = require("body-parser");
+
 // STEP 2: set up a new app that is going to be using the express module
 const app = express();
+
+// STEP 9: allow the web app to use body-parser package
+// use .urlencoded(); to use the information that gets posted to your server from an HTML form
+// add an option called extended and set it to be true to allow us to post nested objects
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // STEP 3: set up a new port that is set to 3000.
 const port = 3000;
 
 // STEP 4: create a home route
 
-app.get("/", function(req, res) {
-    // STEP 6: use res.sendFile(path) to provide a path to the index.html file
-    //__dirname is an environment variable that tells you the absolute path of the directory containing the currently executing file.
-    //__dirname only works in common JS modules DOES NOT WORK IN ESM
-    res.sendFile(__dirname + "/index.html");
-  });
-  
+app.get("/", function (req, res) {
+  // STEP 6: use res.sendFile(path) to provide a path to the index.html file
+  // use directory name and append the index.html file (__dirname + "/index.html")
+  //__dirname is an environment variable that tells you the absolute path of the directory containing the currently executing file.
+  //__dirname only works in common JS modules DOES NOT WORK IN ESM
+  res.sendFile(__dirname + "/index.html");
+});
+
+// STEP 7: Add an app.post method to handle any post requests (needed for forms)
+app.post("/", function (req, res) {
+    console.log(req.body);
+  res.send("Thanks for posting that!");
+});
+
 // STEP 5: Spin up server on port 3000 with app.listen
-  app.listen(port, function() {
-    console.log("Sever started successfully on port 3000.")
-  });
+app.listen(port, function () {
+  console.log("Sever started successfully on port 3000.");
+});
